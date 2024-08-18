30e593ed3a3cc6aa92d4a3075b785fe7887214b0
Ruben Briers <briers.ruben@gmail.com>
2024-08-18 11:35:55 +0200

    Upgrade AHC_SMADataExtractor.ino to JSON formatting

--- AHC_SMADataExtractor.ino
+++ AHC_SMADataExtractor.ino
@@ -8,6 +8,7 @@
 
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
 
 
 #define InvertorSSID "***************"    // The name of your Sunnyboy wifi network
@@ -17,19 +18,13 @@
 
 int httpCode = 0;
 
-//SCAN STRING CODE VARIABLES//
-
-int indexVal = 0;
-int indexValidVals = 0;
-int indexTag = 0;
-
-int validValsArray[5];                    // Output of this code
-int tag = 0;                              // Output of this code
-int val = 0;                              // Output of this code
 
 #define KEY "6100_40263F00"               // desired value (this is the key for power (W))     
 
-///////////////////////////////
+long keyTag = 0;                          // output of this code
+long keyVal = 0;                          // output of this code
+int keyHigh = 0;                          // output of this code
+int keyLow = 0;                           // output of this code
 
 
 
@@ -98,85 +93,44 @@ void loop() {
     if (httpCode == HTTP_CODE_OK){
       payload = http.getString();                                             // response of the server
 
-      //SCAN STRING CODE///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      //SCAN JSON CODE///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-      int indexKeyBegin = payload.indexOf(KEY);                               // search for the desired KEY code in payload
-      String payloadSubString = payload.substring(indexKeyBegin);
-      int indexKeyEnd = payloadSubString.indexOf("},");                       // end of the part of the string designated to KEY...
-      String keyString = payloadSubString.substring(0, indexKeyEnd);          // ...keyString only contains this part of the payload
+      JsonDocument keyFilter;
 
-      indexVal = keyString.indexOf("val");
-      indexValidVals = keyString.indexOf("validVals");
-      indexTag = keyString.indexOf("tag");
-
-
-
-      if (indexValidVals != -1){                                              // get the data designated to "validVals" if it exists
+      keyFilter["result"]["0199-xxxxx42F"][KEY] = true;
       
-        String keyValidValsSubString = keyString.substring(indexValidVals);
-        int indexValidValsValueBegin = keyValidValsSubString.indexOf("[");
-        int indexValidValsValueEnd = keyValidValsSubString.indexOf("]");
+      JsonDocument keyData;
 
-        String validVals = keyValidValsSubString.substring(indexValidValsValueBegin, indexValidValsValueEnd + 1);
+      DeserializationError error = deserializeJson(keyData, payload, DeserializationOption::Filter(keyFilter));
 
-        int arrayIndex = 0;
-        int indexMSD = 1;
+      if (error) {
+        Serial.print("deserializeJson() failed: ");
+        Serial.println(error.c_str());
+        return;
+      }
+
+      if (KEY == "6800_008AA200" || "6800_00832A00"){
+        keyHigh = keyData["1"][0]["high"];
+        keyLow = keyData["1"][0]["low"];
+      }
+
+      if (KEY == "6800_08822000" || "6800_08811F00" || "6800_088A2900" || "6800_08822B00" || "6802_08834500"){
+        JsonArray keyValidVals = keyData["1"][0]["validVals"];                                                         // do something with this array
+        keyTag = keyData["1"][0]["val"][0]["tag"];
+      }
+
+      if (KEY == "6180_08214800" || "6180_08414900" || "6182_08412B00" || "6180_08412800"){
+        keyTag = keyData["1"][0]["val"][0]["tag"];
+      }
+      
+      if (KEY == "6800_10821E00"){
+        const char* keyValChar = keyData["1"][0]["val"];
+      }
+
+      else {
+        keyVal = keyData["1"][0]["val"];
+      }
     
-        for(int i = 1; i < validVals.length(); i++){
-
-          if (validVals[i] == ',' || validVals[i] == ']'){
-            
-            validValsArray[arrayIndex] = (validVals.substring(indexMSD, i)).toInt();          // data from "validVals" is stored in validValsArray
-            indexMSD = i + 1;
-            arrayIndex++;
-          
-          }
-
-        }
-
-        Serial.print("validVals: ");
-
-        for (int i = 0 ; i <= (arrayIndex - 1) ; i++){
-          Serial.print(validValsArray[i]);
-
-          if (i != (arrayIndex - 1)){
-            Serial.print(", ");
-          }       
-
-        }
-
-        Serial.println("");
-      }
-
-      if (indexTag != -1){                                                       // get the data designated to "tag" if it exists
-
-        String keyTagSubString = keyString.substring(indexTag);
-        int indexTagValueBegin = keyTagSubString.indexOf(":");
-        int indexTagValueEnd = keyTagSubString.indexOf("}");
-
-        String tagStr = keyTagSubString.substring(indexTagValueBegin + 1, indexTagValueEnd);
-        tag = tagStr.toInt();                                                   // data from "tag" is stored in tag
-
-        char buffer[40];
-        sprintf(buffer, "Tag: %d", tag);
-        Serial.println(buffer);
-
-      }
-
-      else{                                                                     // get the data designated to "val" if it exists
-
-        String keyValSubString = keyString.substring(indexVal);
-        int indexValValueBegin = keyValSubString.indexOf(":");
-        int indexValValueEnd = keyValSubString.indexOf("}");
-
-        String valStr = keyValSubString.substring(indexValValueBegin + 1, indexValValueEnd);
-        val = valStr.toInt();                                                   // data from "val" is stored in val
-
-        char buffer[40];
-        sprintf(buffer, "Value: %d", val);
-        Serial.println(buffer);
-      
-      }
     }
 
     else{
